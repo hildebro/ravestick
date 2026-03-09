7b54a7787a32c51936ea0caed5777c404337faa8
Alexander Hildebrandt <alex@hilde.dev>
2026-03-09 22:54:53 +0100

    fix duplicate code

--- ravestick/effects.py
+++ ravestick/effects.py
@@ -2,38 +2,38 @@ import numpy as np
 
 
 class BaseEffect:
+    def __init__(self, leds_per_band):
+        self.leds_per_band = leds_per_band
+
+    def _prepare_bands_and_canvas(self, frequency_bars):
+        bands = [
+            np.mean(frequency_bars[0:18]),
+            np.mean(frequency_bars[18:54]),
+            np.mean(frequency_bars[54:64])
+        ]
+        led_colors = np.zeros((3, self.leds_per_band, 3), dtype=int)
+        return bands, led_colors
+
     def process(self, frequency_bars):
         raise NotImplementedError("Subclasses must implement process()")
 
 
 class ThreeBandCyanPulseEffect(BaseEffect):
-    def __init__(self, leds_per_band):
-        self.leds_per_band = leds_per_band
-
     def process(self, frequency_bars):
-        bass = np.mean(frequency_bars[0:18])
-        mids = np.mean(frequency_bars[18:54])
-        highs = np.mean(frequency_bars[54:64])
-
-        bands = [bass, mids, highs]
-        led_colors = np.zeros((3, self.leds_per_band, 3), dtype=int)
+        bands, led_colors = self._prepare_bands_and_canvas(frequency_bars)
 
         for band_idx, amplitude in enumerate(bands):
             # Amplitude is already 0.0 to 1.0, so just multiply by 255 for RGB intensity
             intensity = int(np.clip(amplitude * 255, 0, 255))
-            color = (0, intensity, intensity)
-
-            for led_idx in range(self.leds_per_band):
-                led_colors[band_idx, led_idx] = color
+            led_colors[band_idx, :] = (0, intensity, intensity)
 
         return led_colors
 
 
 class ThreeBandVUMeterEffect(BaseEffect):
     def __init__(self, leds_per_band, bass_threshold=0.6):
-        self.leds_per_band = leds_per_band
+        super().__init__(leds_per_band)
         self.bass_threshold = bass_threshold
-
         self.band_colors = [
             (255, 50, 50),  # Bass
             (50, 255, 50),  # Mids
@@ -41,12 +41,7 @@ class ThreeBandVUMeterEffect(BaseEffect):
         ]
 
     def process(self, frequency_bars):
-        bass = np.mean(frequency_bars[0:18])
-        mids = np.mean(frequency_bars[18:54])
-        highs = np.mean(frequency_bars[54:64])
-
-        bands = [bass, mids, highs]
-        led_colors = np.zeros((3, self.leds_per_band, 3), dtype=int)
+        bands, led_colors = self._prepare_bands_and_canvas(frequency_bars)
 
         for band_idx, amplitude in enumerate(bands):
             if band_idx == 0:
@@ -56,8 +51,7 @@ class ThreeBandVUMeterEffect(BaseEffect):
                 # Amplitude is 0.0 to 1.0, so multiply by total LEDs to get how many to turn on
                 active_leds_count = int(np.clip(amplitude * self.leds_per_band, 0, self.leds_per_band))
 
-            for led_idx in range(active_leds_count):
-                led_colors[band_idx, led_idx] = self.band_colors[band_idx]
+            led_colors[band_idx, :active_leds_count] = self.band_colors[band_idx]
 
         return led_colors
 
